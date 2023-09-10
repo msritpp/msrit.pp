7b3e68720c801d3e3baabdedd4c655edb54a5fcc
msritpp <142683367+msritpp@users.noreply.github.com>
2023-09-10 10:46:42 -0700

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -2,13 +2,3 @@ function change(){
     let color = prompt('Enter a color to change background color:')
     document.body.style.background = color
 }
-
-//translate//
-
-function googleTranslateElementInit(){
-    new google.translate.TranslateElement(
-        {pageLanguage :'en',},
-        'google_translate_element'
-        
-    );
-}

ef9be600aab1af1855ea807c78bc032a41900316
msritpp <142683367+msritpp@users.noreply.github.com>
2023-09-16 09:02:22 -0700

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -1,4 +1,7 @@
-function change(){
+function click(){
     let color = prompt('Enter a color to change background color:')
     document.body.style.background = color
 }
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('button').onclick = click
+})

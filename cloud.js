9749048f487a1814a55f2f2d29c5974bb19239ab
msritpp <142683367+msritpp@users.noreply.github.com>
2023-09-19 13:00:12 +0530

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -1,11 +1,11 @@
 //switch theme//
 
 function click(){
-  let color = prompt('Enter a color to change background color:')
-  document.body.style.background = color
+  let color = prompt('Enter a color to change background color:');
+  document.body.style.background = color;
 }
 document.addEventListener('DOMContentLoaded',function(){
-  document.querySelector('button').onclick = click
+  document.querySelector('button').onclick = click;
 })
 
 //switch//
@@ -33,5 +33,5 @@ function togglehide(){
 }
 
 document.addEventListener('DOMContentLoaded',function(){
-  document.querySelector('#hide').onclick = togglehide
+  document.querySelector('#hide').onclick = togglehide;
 })
